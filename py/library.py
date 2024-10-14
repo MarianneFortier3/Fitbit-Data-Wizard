98a30764fff78bb302d95a975bb9770667a799a6
MarianneFortier3 <marianne.fortier3@usherbrooke.ca>
2024-10-14 16:28:05 -0400

    update

--- py/library.py
+++ py/library.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 # -*- coding: utf-8 -*-
 """
 Created on Sun Dec 31 14:06:35 2023
@@ -236,3 +237,240 @@ def export_data(root_path, w, startDate, endDate, birth_year, keyword = ['steps'
 
 
 
+=======
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 31 14:06:35 2023
+
+@author: Marianne
+"""
+
+# Fonctions pour extraire les données des montres
+
+import pandas as pd
+from pandas.tseries.offsets import DateOffset
+import numpy as np
+import os
+import sys
+from datetime import datetime, timedelta
+
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+# Find the files starting with the keyword
+def find_json_files(file_list, keyword, dates):
+    matched_files = []
+    for d in dates:
+        try:
+            for f in file_list:
+                if f.startswith(keyword+'-'+d):
+                    matched_files.append(f)
+        except:
+            pass
+    matched_files = np.array(matched_files).flatten()
+    
+    return(matched_files)
+
+# Extract active zones
+def open_and_concat_active(data_path,file_list,keyword,birth_year,startDate,endDate):
+    
+    to_concat = []
+    
+    if len(file_list) != 0:
+    
+        for f in file_list:
+            df = pd.read_json(data_path+f)
+            values = pd.DataFrame.from_records(df['value'].values)
+            df.drop(labels='value',axis=1,inplace=True)
+            df['bpm']=values['bpm']
+            
+            df.set_index('dateTime',inplace=True)
+            df.rename(columns={'value':keyword},inplace=True)
+            
+            to_concat.append(df)
+            
+        original = pd.concat(to_concat)
+        original = original.loc[startDate:endDate-timedelta(seconds=1)]
+        result = original.resample('1min').mean()
+        result['écart-type'] = original.resample('1min').std(ddof=0)
+
+        fcm = 220 - (datetime.now().year - int(birth_year))
+        result['zone'] = None
+        result['zone'].loc[result['bpm']<0.5*fcm] = 'sédentaire'
+        result['zone'].loc[(result['bpm']>=0.5*fcm) & (result['bpm']<0.7*fcm)] = 'légère'
+        result['zone'].loc[(result['bpm']>=0.7*fcm) & (result['bpm']<0.85*fcm)] = 'modérée'
+        result['zone'].loc[(result['bpm']>=0.85*fcm)] = 'intense'
+        result.index.name = 'datetime'
+        result.dropna(inplace=True)
+    else:
+        result=pd.DataFrame()
+
+    return(result.round(2))
+ 
+# Extract steps    
+def open_and_concat_steps(data_path,file_list,keyword,startDate,endDate):
+    
+    to_concat = []  
+    
+    if len(file_list) != 0:
+    
+        for f in file_list :
+            df = pd.read_json(data_path+f)
+            df.set_index('dateTime',inplace=True)
+            df.rename(columns={'value':keyword},inplace=True)
+            
+            to_concat.append(df)
+        
+        result = pd.concat(to_concat)
+        result = result.loc[startDate:endDate-timedelta(seconds=1)]
+        result = result.resample('1min').sum()
+        result.index.name = 'datetime'
+        result.dropna(inplace=True)
+        
+        #result = result.loc[result[keyword]!=0]
+    else:
+        result=pd.DataFrame()
+    
+    return(result)
+
+# Extract daily stats
+def daily_stats(result_min,startDate,endDate,keyword=['steps','heart_rate']):
+    
+    df = []
+
+    if 'steps' in keyword:
+        # Compute steps
+        steps = result_min['steps'].resample('D').sum()
+        df.append(steps)
+    if 'heart_rate' in keyword:
+        # Compute zones
+        result_min['day'] = result_min.index.normalize()
+        zone = result_min.groupby(['day','zone'])['bpm'].count().unstack(level=1)
+        df.append(zone)
+        # Compute bpm stats
+        bpm_gb = result_min.groupby('day')['bpm'].agg(['max','min','mean','std'])
+        bpm_gb.rename(columns = {'max':'bpm_max','min':'bpm_min','mean':'bpm_moyen','std':'bpm_et'},inplace=True)
+        df.append(bpm_gb)
+    
+    result_day = pd.concat(df,axis=1)
+    day_list = pd.date_range(startDate.normalize(),endDate-timedelta(seconds=1),freq='D')
+    for d in day_list:
+        if d not in result_day.index:
+            result_day.loc[d] = None
+    if endDate.hour == 0:
+        endDate = endDate - timedelta(minutes=1)
+        
+    start_date_index = pd.date_range(startDate.normalize(),endDate,freq='D')
+    if startDate not in start_date_index:
+        start_date_index = start_date_index.delete(0)
+        start_date_index = start_date_index.insert(0,startDate)
+        
+    end_date_index = pd.date_range(startDate.strftime('%Y%m%d 23:59:00'),endDate.strftime('%Y%m%d 23:59:00'),freq='D')
+    if endDate not in end_date_index:
+        end_date_index = end_date_index.delete(len(end_date_index)-1)
+        end_date_index = end_date_index.insert(len(end_date_index),endDate)
+
+    
+    result_day.insert(0,'endDate',end_date_index)
+    result_day.insert(0,'startDate',start_date_index) 
+        
+    return(result_day.reset_index(drop=True))
+
+# Make a liste with all the watches in the directory
+def list_all_watch(root_path):
+    
+    watch_list = []
+    
+    for watch in next(os.walk(root_path))[1]:
+        watch_list.append(watch)
+    
+    return(watch_list)
+
+# Extract what the client wants for one watch
+def export_data(root_path, w, startDate, endDate, birth_year, keywords = ['steps','heart_rate']):  
+    keyword = keywords[:]
+    try:
+        startDate = pd.to_datetime(startDate,format='%m/%d/%y %H:%M')
+        endDate = pd.to_datetime(endDate,format='%m/%d/%y %H:%M')
+        
+        data_path = root_path + '/' + w + '/Fitbit/Global Export Data/'  
+        all_files_list = os.listdir(data_path)
+            
+        to_extract_files = {}
+            
+        for k in keyword :
+            if k == 'heart_rate':
+                dates = pd.date_range(startDate.normalize()-timedelta(days=1),endDate.normalize()+timedelta(days=1),freq='D')
+                dates = dates.strftime('%Y-%m-%d')
+                to_extract_files[k] = find_json_files(all_files_list,k,dates)
+            elif k == 'steps':
+                dates = pd.date_range(startDate.normalize()-DateOffset(months=1),endDate.normalize()+DateOffset(months=1),freq='D')
+                dates = dates.strftime('%Y-%m')
+                to_extract_files[k] = find_json_files(all_files_list,k,np.unique(dates))    
+        concatenated_json = []
+        date_list = []
+        no_data=True
+            
+        for k in keyword :
+            if k == 'steps':
+                concatenated_file = open_and_concat_steps(data_path, to_extract_files[k], k, startDate, endDate)
+            elif k == 'heart_rate':
+                concatenated_file = open_and_concat_active(data_path, to_extract_files[k], k, birth_year, startDate, endDate)
+            
+            concatenated_file.set_index(pd.to_datetime(concatenated_file.index),inplace=True)
+            
+            concatenated_json.append(concatenated_file)
+            date_list.extend(concatenated_file.index)
+            if len(concatenated_file)==0:
+                keyword.remove(k)
+            else:
+                no_data=False
+
+        if no_data :
+            return(['No data'])
+            
+        dateList = np.unique(date_list)
+        dateList.sort()
+            
+        result_file = pd.DataFrame(data=None)     
+        min_date = min(dateList)
+        max_date = max(dateList)
+        result_file['Date'] = pd.date_range(min_date,max_date)
+        
+        result_file = pd.concat(concatenated_json,axis=1)
+    
+        result_file = result_file.loc[startDate:endDate]
+        result_file.dropna(inplace=True,how='all')
+        result_file = result_file.round(4)
+            
+        pretty_w = w.replace(' ','_')
+        
+        excel_file = root_path + '/' + pretty_w +'_'+startDate.strftime('%Y%m%d_%H%M')+'_'+endDate.strftime('%Y%m%d_%H%M')+'.xlsx'
+    
+        excel_writer = pd.ExcelWriter(excel_file,mode='w',
+                                      datetime_format='YYYY-MM-DD HH:MM:SS',
+                                      engine="openpyxl")
+
+        with excel_writer as writer:  
+            result_file.to_excel(writer, sheet_name='Par minute',
+                                 index_label='Dates') 
+        
+        # Add daily results
+        result_daily = daily_stats(result_file,startDate,endDate,keyword)
+        result_daily = result_daily.round(4)
+        excel_writer = pd.ExcelWriter(excel_file,mode='a',
+                                      datetime_format='YYYY-MM-DD HH:MM:SS',
+                                      engine="openpyxl")
+        with excel_writer as writer:  
+            result_daily.to_excel(writer, sheet_name='Total',index=False)         
+
+        return(['Success'])
+    except Exception as error:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        print(exc_tb.tb_lineno)
+        return(['Error',error])
+    
+
+
+
+>>>>>>> Stashed changes
--- py/main.py
+++ py/main.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 # -*- coding: utf-8 -*-
 """
 Created on Tue Jan 16 12:55:04 2024
@@ -228,3 +229,235 @@ credit = tk.Label(window,text='Application développée par Marianne Fortier',fo
 credit.grid(column=3,row=r,columnspan=5, padx=10, pady=25, sticky='e')
 
 window.mainloop()
+=======
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 16 12:55:04 2024
+
+@author: Marianne
+"""
+
+import sys
+sys.path.append('./_internal/')
+
+from library import list_all_watch, export_data
+import tkinter as tk
+from tkinter import filedialog as fd
+import tkinter.scrolledtext as st
+import time
+from tkcalendar import Calendar,DateEntry
+import os
+
+import warnings
+warnings.filterwarnings('ignore') 
+
+basedir = os.path.dirname(__file__)
+
+# Create the window
+window = tk.Tk()
+window.title("Fitbit Data Wizard")
+window.geometry("800x500")
+window.iconbitmap(os.path.join(basedir, "wizard_emoji.ico"))
+
+r=0
+
+# Add title and instructions
+title = tk.Label(window,text = "Bienvenue sur Fitbit Data Wizard ! 🪄",
+                 font=("Helvetica", 16))
+title.grid(column=0,row=r,columnspan=10,padx=225)
+
+r+=1
+
+inst1 = tk.Label(window,
+                 text = "Entrez le répertoire de votre dossier avec toutes vos montres,\nchoisissez la date de début et de fin et sélectionnez les métriques à exporter.",
+                 font = ("Helvetica", 10))
+inst1.grid(column=0,row=r,columnspan=10)
+
+r+=1
+
+# Enter the root path
+path = tk.Label(window,text="Votre dossier : ",font=("Helvetica", 10))
+path.grid(column=0,row=r, pady=40, padx=30, sticky='w',columnspan=3)
+
+pathEntry = tk.Entry(window, width=70)
+pathEntry.grid(column=0, row=r, pady=40, padx=125, columnspan=6, sticky='w')
+
+def browse():
+    directory=fd.askdirectory()
+    pathEntry.delete(0,tk.END)
+    pathEntry.insert(0,directory)
+
+browseButton = tk.Button(window,text='Mes dossiers',command=browse,bg='lightblue')
+browseButton.grid(column=4,columnspan=6,padx=40,row=r,pady=40, sticky='w')
+r+=1
+
+# Enter the start and end 
+option = tk.Label(window, text='Options :',font=("Helvetica", 10))
+option.grid(column=0, row=r, pady=5, padx=30, sticky='w')
+r+=1
+
+date1 = tk.Label(window,text='Date de début : ')
+startcal = DateEntry(window, width=12, year=2024)
+timetxt1 = tk.Label(window,text="HH:MM")
+hourstart = tk.Entry(window,width=3)
+twodots1 = tk.Label(window,text=':')
+minstart = tk.Entry(window,width=3)
+
+date2 = tk.Label(window,text='Date de fin : ')
+endcal = DateEntry(window, width=12, year=2024)
+timetxt2 = tk.Label(window,text="HH:MM")
+hourend = tk.Entry(window,width=3)
+twodots2 = tk.Label(window,text=':')
+minend = tk.Entry(window,width=3)
+
+def showCalendar(r) :
+    
+    date1.grid(column=0,row=r,pady=5,sticky='w',padx=35,columnspan=3)
+    startcal.grid(column=0, row=r, pady=5, columnspan=3, padx=122, sticky='w')
+    timetxt1.grid(column=1,row=r,pady=5,columnspan=2,sticky='w')
+    hourstart.grid(column=1,row=r,padx=55,columnspan=3,sticky='w')
+    twodots1.grid(column=1,row=r,padx=80,columnspan=2,sticky='w')
+    minstart.grid(column=1,row=r,padx=93,columnspan=3,sticky='w')
+        
+    date2.grid(column=0,row=r+1,pady=5,padx=35,sticky='w',columnspan=3)
+    endcal.grid(column=0, columnspan=3, padx=122, row=r+1, pady=5,sticky='w')
+    timetxt2.grid(column=1,row=r+1,pady=5,columnspan=2,sticky='w')
+    hourend.grid(column=1,row=r+1,padx=55,columnspan=3,sticky='w')
+    twodots2.grid(column=1,row=r+1,padx=80,columnspan=2,sticky='w')
+    minend.grid(column=1,row=r+1,padx=93,columnspan=3,sticky='w')
+
+showCalendar(r)
+r+=5
+
+# Les métriques
+useSteps = tk.BooleanVar()
+useActive = tk.BooleanVar()
+
+metrics = tk.Label(window, text='Métriques :')
+metrics.grid(column=0, row=r, pady=5,padx=35, sticky='w')
+r+=1
+
+active = tk.Checkbutton(window, text='Zones actives',
+                      variable=useActive, onvalue=True, offvalue=False)
+active.select()
+active.grid(column=0, row=r, pady=5,padx=35,sticky='w')
+
+steps = tk.Checkbutton(window, text='Pas',
+                       variable=useSteps, onvalue=True, offvalue=False)
+steps.select()
+steps.grid(column=1, row=r, pady=5, sticky='w')
+
+r+=1
+
+birth = tk.Label(window,text='Année de naissance :')
+birth.grid(column=0,row=r, pady=5, padx=35, sticky='w',columnspan=3)
+yr_birth = tk.Entry(window,width=10)
+yr_birth.grid(column=0,row=r,pady=5,padx=152,sticky='w',columnspan=3)
+
+r+=1
+
+def openNewWindow():
+    newWindow = tk.Toplevel(window)
+    newWindow.title("Fitbit Data Wizard")
+    newWindow.geometry("500x250")
+    newWindow.iconbitmap(os.path.join(basedir, "wizard_emoji.ico"))
+    
+    label = tk.Label(newWindow, 
+                     text="Progression :", 
+                     font = ("Helvetica", 10))
+    label.pack(anchor='w',padx=25)
+    
+    return(newWindow)
+
+def defineKeywords():
+    
+    keywords = []
+    
+    if useActive.get():
+        keywords.append('heart_rate')
+    if useSteps.get():
+        keywords.append('steps')     
+        
+    return(keywords)
+    
+def useInput():
+    
+    newWindow = openNewWindow()
+    
+    start = time.time()
+    
+    root_path = pathEntry.get()
+    watch_list = list_all_watch(root_path)
+    
+    progress = st.ScrolledText(newWindow,    
+                                      width = 60,  
+                                      height = 10,  
+                                      font = ("Helvetica", 10)) 
+    progress.pack(padx=10,pady=10)
+    
+    startDate = startcal.get()
+    endDate = endcal.get()
+
+    startTime = str(hourstart.get())+':'+str(minstart.get())
+    endTime = str(hourend.get())+':'+str(minend.get())
+    
+    birth_year = yr_birth.get()
+    
+    keywords = defineKeywords()
+    
+    # Afficher les options dans la boite de texte
+    for w in watch_list:
+        print(w)
+        verif = export_data(root_path,w,startDate+' '+startTime,endDate+' '+endTime,birth_year,keywords=keywords)        
+        print(verif)
+        if verif[0] == 'Error':
+            break
+        elif verif[0] == 'No data':
+            message = "-------------------------------------------------------------------\n" + "{w} n'a aucune donnée pour les dates entrées.\n".format(w=w) + "-------------------------------------------------------------------\n"
+            progress.insert(tk.END, message)
+            progress.see("end")
+            progress.update()
+        else:
+            message = "-------------------------------------------------------------------\n" + "L'extraction de {w} est terminée.\n".format(w=w) + "-------------------------------------------------------------------\n"
+            progress.insert(tk.END, message)
+            progress.see("end")
+            progress.update()
+    
+    end = time.time()
+    
+    if verif[0] == 'Error':
+        progress.insert(tk.END,"\nERREUR!",'error')
+        progress.tag_config('error', foreground='red')
+        message = "\nUne erreur est survenue. Assurez-vous d'avoir bien entré vos paramètres avant de relancer le programme et de regarder la vidéo explicative.\nSi l'erreur persiste, veuillez envoyer à Marianne Fortier vos fichiers,\nles paramètres entrés et copier-coller le texte d'erreur plus bas au\nmarianne.fortier@gmail.com."
+        progress.insert(tk.END,message)
+        message = "\n\nL'erreur est "+str(verif[1])
+        progress.insert(tk.END,message)
+    else:
+        progress.insert(tk.END,"\nSUCCÈS!",'success')
+        progress.tag_config('success', foreground='green')
+        message = "\nL'extraction s'est terminée en "+ time.strftime("%H h %M min %S sec.", time.gmtime(end-start))
+        progress.insert(tk.END,message)
+        progress.insert(tk.END,"\nVos fichiers se trouvent dans " + root_path)
+        progress.see("end")
+
+    progress.configure(state ='disabled')
+    
+    finishButton = tk.Button(newWindow,
+                       text="OK",
+                       command=newWindow.destroy,
+                       bg="lightblue",
+                       padx=15,
+                       font=("Helvetica", 8))
+    finishButton.pack(pady=5,anchor='n')
+    
+    
+button = tk.Button(window,text="Commencer la magie ✨",command=useInput,bg="lightblue")
+button.grid(column=0,row=r,columnspan=6,pady=30)
+
+r+=1
+
+credit = tk.Label(window,text='Application développée par Marianne Fortier',font=("Helvetica", 8, "italic"))
+credit.grid(column=3,row=r,columnspan=5, padx=10, pady=25, sticky='e')
+
+window.mainloop()
+>>>>>>> Stashed changes
